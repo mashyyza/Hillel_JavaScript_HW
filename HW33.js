6047735c859b0133171abfd020bc8574359d326b
Mariia <mashyyza@gmail.com>
2023-10-25 11:31:46 +0300

    correction of mistakes in the code

--- HW33.js
+++ HW33.js
@@ -1,39 +1,53 @@
-class HTMLAnchor {
 
-    constructor(x, y, width, height) {
+class HTMLElement {
+    constructor(className, id) {
+        console.log("---- HTMLElement constructor ----", this);
+        this.className = className;
+        this.id = id;
+    }
+
+    render() {
+        return "Tag rendered from HTMLElement.prototype";
+    }
+
+    rotate() {
+        console.log("Common rotate", this);
+    }
+}
+
+class HTMLAnchor extends HTMLElement {
+    constructor(x, y, width, height, className, id) {
+        super(className, id);
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
     }
-  
+
     render() {
-        return `<a href="#"></a>`;
+        return `<a href="#" class="${this.className}" id="${this.id}" style="position: absolute; left: ${this.x}px; top: ${this.y}px; width: ${this.width}px; height: ${this.height}px;"></a>`;
     }
-  
-    rotate(angle) {
-        this.x = this.x * Math.cos(angle) - this.y * Math.sin(angle);
-        this.y = this.x * Math.sin(angle) + this.y * Math.cos(angle);
-    }
-  
-  }
-  
-  class HTMLElementInput extends HTMLAnchor {
-  
-    constructor(x, y, width, height, type, disabled) {
-        super(x, y, width, height);
+}
+
+class HTMLElementInput extends HTMLAnchor {
+    constructor(x, y, width, height, className, id, type, disabled) {
+        super(x, y, width, height, className, id);
         this.type = type;
         this.disabled = disabled;
     }
-  
+
     render() {
         const disabledClass = this.disabled ? "disabled" : "";
-  
-        return `<input type="${this.type}" class="${disabledClass}" x="${this.x}" y="${this.y}" width="${this.width}" height="${this.height}" />`;
+        return `<input type="${this.type}" class="${this.className} ${disabledClass}" id="${this.id}" style="position: absolute; left: ${this.x}px; top: ${this.y}px; width: ${this.width}px; height: ${this.height}px;" />`;
     }
-  
-  }
-  
-  const input = new HTMLElementInput(35, 20, 102, 90, "text", true);
-  console.log(input.render());
+
+    rotate() {
+        console.log("Input rotate", this);
+    }
+}
+
+const input = new HTMLElementInput(10, 20, 30, 90, "input-class", "input-id", "text", true);
+console.log(input.render());
+
+
   
